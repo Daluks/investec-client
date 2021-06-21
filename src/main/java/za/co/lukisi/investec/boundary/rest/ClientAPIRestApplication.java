55341536886f558a0d8d7dd635ebbd867201a0c5
lukis <lukisim@gmail.com>
2021-06-21 13:51:53 +0200

    OpenAPI config

--- src/main/java/za/co/lukisi/investec/boundary/rest/ClientAPIRestApplication.java
+++ src/main/java/za/co/lukisi/investec/boundary/rest/ClientAPIRestApplication.java
@@ -1,8 +1,12 @@
 package za.co.lukisi.investec.boundary.rest;
 
+import io.swagger.jaxrs.config.BeanConfig;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  *
@@ -10,4 +14,21 @@ import javax.ws.rs.core.Application;
 @ApplicationPath("/api")
 @ApplicationScoped
 public class ClientAPIRestApplication extends Application {
+    public ClientAPIRestApplication(){
+        BeanConfig beanConfig = new BeanConfig();
+        beanConfig.setVersion("1.0.2");
+        beanConfig.setSchemes(new String[]{"http"});
+        beanConfig.setHost("localhost:8080");
+        beanConfig.setBasePath("investec-client-api/api");
+        beanConfig.setResourcePackage("io.swagger.resources");
+        beanConfig.setScan(true);
+    }
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new HashSet();
+        resources.add(io.swagger.jaxrs.listing.ApiListingResource.class);
+        resources.add(io.swagger.jaxrs.listing.SwaggerSerializers.class);
+        return super.getClasses();
+    }
 }
